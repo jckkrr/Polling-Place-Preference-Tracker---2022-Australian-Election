7e572c61f998ffdbd99e96751ceac81d3db23aa0
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-02-22 14:55:50 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ headers = {
     "content-type": "application/json"
 }
 
-css = 'body, html, p, h1, .st-emotion-cache-1104ytp h1, [class*="css"] {font-family: "Inter", sans-serif;}'
+css = 'body, p, h1, .st-emotion-cache-1104ytp h1, [class*="css"] {font-family: "Inter", sans-serif;}'
 st.markdown( f'<style>{css}</style>' , unsafe_allow_html= True)
 
 
