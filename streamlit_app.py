1964b1ff03de0853b27bd9d672070409ab9e7eba
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-02-22 11:53:44 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -169,7 +169,7 @@ df_prefdist = prefdist(chosen_df, chosen_state, chosen_electorate, chosen_pollin
 
 plotter(chosen_df, chosen_state, chosen_electorate, chosen_pollingplace)
 
-st.table(df_prefdist.style.format("{:,.0f}"))
+#st.table(df_prefdist.style.format("{:,.0f}"))
 
 cell_hover = {  # for row hover use <tr> instead of <td>
     'selector': 'td:hover',
@@ -183,9 +183,13 @@ headers = {
     'selector': 'th:not(.index_name)',
     'props': 'background-color: #fefefe; color: #181818; font-size: 10pt; text-align:right; font-weight: bold'
 }
-s = df_prefdist.style.format()
+s = df_prefdist.style.format().set_properties(**{'font-size': '10pt'}).format("{:,.0f}")
 s.set_table_styles([cell_hover, index_names, headers])
 st.table(s.set_table_styles([cell_hover, index_names, headers]))
+
+
+
+
 
 cell_hover = {  # for row hover use <tr> instead of <td>
     'selector': 'td:hover',
